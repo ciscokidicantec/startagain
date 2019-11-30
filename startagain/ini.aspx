81855ae411af474f7dc0963670436004feb4e566
mario wakeham <mariowakeham@outlook.com>
2019-11-30 23:33:53 +0000

    Add project files.

---
+++ startagain/ini.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ini.aspx.cs" Inherits="startagain.ini" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+        <br />
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <form id="form1" runat="server">
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="imageindex" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="imageindex" HeaderText="imageindex" ReadOnly="True" SortExpression="imageindex" />
+                    <asp:BoundField DataField="myguid" HeaderText="myguid" SortExpression="myguid" />
+                    <asp:BoundField DataField="originalfilename" HeaderText="originalfilename" SortExpression="originalfilename" />
+                    <asp:BoundField DataField="imagesizeKbytes" HeaderText="imagesizeKbytes" SortExpression="imagesizeKbytes" />
+                    <asp:BoundField DataField="savedondiskfilename" HeaderText="savedondiskfilename" SortExpression="savedondiskfilename" />
+                    <asp:BoundField DataField="inserteddate" HeaderText="inserteddate" SortExpression="inserteddate" />
+                    <asp:BoundField DataField="postalcodeplace" HeaderText="postalcodeplace" SortExpression="postalcodeplace" />
+                </Columns>
+            </asp:GridView>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:estateporrtalConnectionString %>" ProviderName="<%$ ConnectionStrings:estateporrtalConnectionString.ProviderName %>" SelectCommand="SELECT * FROM images"></asp:SqlDataSource>
+        </p>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ startagain/ini.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+
+
+namespace startagain
+{
+    public partial class ini : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string connStr = ConfigurationManager.ConnectionStrings["estateporrtalConnectionString"].ConnectionString;
+            MySqlConnection myConnection = new MySqlConnection(connStr);
+            MySql.Data.MySqlClient.MySqlCommand cmd;
+
+            try
+            {
+
+                string CmdString = "INSERT INTO estateporrtal.images(" +
+                    "imageindex," +
+                    "image," +
+                    "myguid," +
+                    "originalfilename," +
+                    "imagesizeKbytes," +
+                    "savedondiskfilename," +
+                    "inserteddate) " +
+                    "VALUES(" +
+                    "@imageindex," +
+                    "@image," +
+                    "@myguid," +
+                    "@originalfilename," +
+                    "@imagesizeKbytes," +
+                    "@savedondiskfilename," +
+                    "@inserteddate)";
+
+
+                cmd = new MySqlCommand(CmdString, myConnection);
+                cmd.Connection = myConnection;
+
+                byte[] imagebytes = new byte[1];
+                imagebytes[0] = 3;
+
+
+
+                cmd = new MySqlCommand(CmdString, myConnection);
+                cmd.Parameters.Add("@imageindex", MySqlDbType.VarChar,36);
+                cmd.Parameters.Add("@image", MySqlDbType.LongBlob);
+                cmd.Parameters.Add("@myguid", MySqlDbType.VarChar, 36);
+                cmd.Parameters.Add("@originalfilename", MySqlDbType.VarChar, 36);
+                cmd.Parameters.Add("@imagesizeKbytes", MySqlDbType.Int32);
+                cmd.Parameters.Add("@savedondiskfilename", MySqlDbType.VarChar, 255);
+                cmd.Parameters.Add("@inserteddate", MySqlDbType.DateTime);
+
+                cmd.Parameters["@imageindex"].Value = Guid.NewGuid();
+                cmd.Parameters["@image"].Value = imagebytes;
+                cmd.Parameters["@myguid"].Value = Guid.NewGuid();
+                cmd.Parameters["@originalfilename"].Value = Guid.NewGuid();
+                cmd.Parameters["@imagesizeKbytes"].Value = 34567;
+                cmd.Parameters["@savedondiskfilename"].Value = "abc";
+                cmd.Parameters["@inserteddate"].Value = DateTime.Now;
+
+                myConnection.Open();
+
+
+                int RowsAffected = cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error Message = " + ex.Message);
+            }
+            myConnection.Close();
+            myConnection.Dispose();
+        }
+    }
+}
